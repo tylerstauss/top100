9a9af4bc8c945c95da58a6abc543760130b528c1
Tyler Stauss <tyler.e.stauss@gmail.com>
2014-06-20 10:45:00 -0700

    push jumbotron to heroku

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,16 +6,16 @@ class MoviesController < ApplicationController
 		# @movie_link = Movie.populate_db
 		@movies = Movie.order('rating desc')
 
-		# @movies.each do |movie|
-		# 	movie.get_imdb
-		# end
-		# @movies.each do |movie|
-		# 	movie.find_imdb if movie.imdb.nil?
-		# end
+		@movies.each do |movie|
+			movie.get_imdb
+		end
+		@movies.each do |movie|
+			movie.find_imdb if movie.imdb.nil?
+		end
 
-		# @movies.each do |movie|
-		# 	movie.get_rating if movie.imdb
-		# end
+		@movies.each do |movie|
+			movie.get_rating if movie.imdb
+		end
 
 
 	end
